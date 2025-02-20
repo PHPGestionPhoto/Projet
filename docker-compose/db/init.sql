2ad050e7cc0d4641167de40a687bf8456cd836a8
Silvus <hugo.combe.84@gmail.com>
2025-02-20 20:00:52 +0100

    fix : docker  need to do CI/CD

--- docker-compose/db/init.sql
+++ docker-compose/db/init.sql
@@ -1,7 +1,7 @@
 create table USERS
 (
     id         SERIAL      not null
-        constraint ID
+        constraint USERS_ID_PK
             primary key,
     first_name    varchar(64) not null,
     last_name    varchar(64) not null,
@@ -13,42 +13,43 @@ create table USERS
 create table GROUPS
 (
     id          SERIAL      not null
-        constraint ID
+        constraint GROUPS_ID_PK
             primary key,
     name        varchar(64) not null,
     description varchar(256),
     owner_id    integer     not null,
-        constraint GROUPS_USERS_ID_TO_GROUPS_OWNER_ID
-            references users
+    constraint GROUPS_USERS_ID_TO_GROUPS_OWNER_ID
+        foreign key (owner_id) references USERS(id)
 );
 
 create table PHOTOS
 (
     uuid        varchar(36)                         not null
-        constraint PHOTOS_pk
+        constraint PHOTOS_UUID_PK
             primary key,
     title       varchar(64)                         not null,
     description varchar(256)                        not null,
     likes       integer   default 0                 not null,
     visibility  integer   default 1                 not null,
-    owner_id    integer                             not null
-        constraint FK_PHOTOS_USERS_ID_TO_PHOTOS_OWNER_ID
-            references users,
-    group_id    integer                             not null
-        constraint FK_PHOTOS_GROUPS_ID_TO_PHOTOS_GROUP_ID
-            references GROUPS (id),
+    owner_id    integer                             not null,
+    constraint FK_PHOTOS_USERS_ID_TO_PHOTOS_OWNER_ID
+        foreign key (owner_id) references USERS(id),
+    group_id    integer                             not null,
+    constraint FK_PHOTOS_GROUPS_ID_TO_PHOTOS_GROUP_ID
+        foreign key (group_id) references GROUPS(id),
     created_at  timestamp default CURRENT_TIMESTAMP not null
 );
 
 create table USER_FOLLOW_GROUPS
 (
-    id         SERIAL                              not null,
-    user_id    integer                             not null
-        constraint FK_USERS_ID_TO_USER_FOLLOW_GROUPS_USER_ID
-            references users,
-    group_id   integer                             not null
-        constraint FK_GROUPS_ID_TO_USER_FOLLOW_GROUPS_GROUP_ID
-            references GROUPS (id),
+    id         SERIAL                              not null
+        constraint USER_FOLLOW_GROUPS_ID_PK
+            primary key,
+    user_id    integer                             not null,
+    constraint FK_USERS_ID_TO_USER_FOLLOW_GROUPS_USER_ID
+        foreign key (user_id) references USERS(id),
+    group_id   integer                             not null,
+    constraint FK_GROUPS_ID_TO_USER_FOLLOW_GROUPS_GROUP_ID
+        foreign key (group_id) references GROUPS(id),
     created_at timestamp default CURRENT_TIMESTAMP not null
 );
-
